b25846dff6a74155c3c40fbfd8b6d2ab1aaacc54
Felipe Nelli Martins <fn.martins@outlook.com>
2023-06-15 00:58:51 -0300

    Adição Sistema de Logout
    Encerramento da Sessão.

--- manutencao.php
+++ manutencao.php
@@ -38,7 +38,7 @@ if(!isset($_SESSION["userId"])) {
                     <li class='ms-5'><a href='#'>
                             <?php echo $_SESSION["username"] ?>
                         </a></li>
-                    <li class="ms-2"><a href="/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                    <li class="ms-2"><a href="./logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                 fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 1.5 16 16">
                                 <path fill-rule="evenodd"
                                     d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
